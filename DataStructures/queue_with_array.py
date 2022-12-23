917b58cf8117aea0b60c0fb9869a061cff05012f
Idris Adebisi <idris.adebisi@yahoo.com>
2022-12-23 16:22:59 +0000

    added new files

---
+++ DataStructures/queue_with_array.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Queue:
+    def __init__(self):
+        self.first = None
+        self.last = None
+        self.length = 0
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def peek(self):
+        return self.first
+
+    def enqueue(self, value):
+        if self.first is None:
+            self.first = new_node
+            self.last = new_node
+        else:
+            self.last.next = new_node
+            self.last = new_node
+        self.length += 1
+        return self
+
+    def dequeue(self):
+        if self.length == 0:
+            return None
+        if self.first == self.last:
+            self.last = None
+        temp = self.first
+        self.first = self.first.next
+        self.length -= 1
+        return self
+
+
+queue = Queue()
+queue.enqueue('Python')
+queue.enqueue('Java')
+queue.enqueue('Sql')
+queue.dequeue()
+queue.peek()
